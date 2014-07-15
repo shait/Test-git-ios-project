f034d82d9846e374e809122f58da9bd9f44ba786
shait <bayaliev.mar@yandex.ru>
2014-07-15 23:18:16 +0400

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  gitTest
+//
+//  Created by Shait on 15.07.14.
+//  Copyright (c) 2014 bayaliev.mar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  gitTest
+//
+//  Created by Shait on 15.07.14.
+//  Copyright (c) 2014 bayaliev.mar. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
